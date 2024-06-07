7f8838982f266bea959c7c19cc6a784bf4bdf0d7
gyb <gyb984571377@gmail.com>
2024-06-07 19:28:03 +0800

    [343] 整数拆分

---
+++ src/343.integer-break.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=343 lang=javascript
+ * @lcpr version=30203
+ *
+ * [343] 整数拆分
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function(n) {
+  const dp = new Array(n+1).fill(0)
+  dp[2]=1
+  for (let i = 3; i <= n; i++) {
+    for (let j = 1; j <= Math.floor(i / 2); j++) {
+      dp[i] = Math.max(dp[i], Math.max((i-j)*j, dp[i-j]*j))
+    }
+  }
+  return dp[n]
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 10\n
+// @lcpr case=end
+
+ */
+

8c87a1288260b00b7d5fc8d5a142ace219e4e056
gyb <gyb984571377@gmail.com>
2024-06-10 17:55:23 +0800

    [40] 组合总和 II

--- src/40.combination-sum-ii.js
+++ src/40.combination-sum-ii.js
@@ -16,49 +16,35 @@
  * @return {number[][]}
  */
 var combinationSum2 = function(candidates, target) {
-  const res = []
-  const path = []
-  candidates.sort((a, b) => a - b)
-  function notExistRes(path) {
-    const len = path.length
-    for (let i = 0; i < res.length; i++) {
-      if (res[i].length !== len) {
-        continue
-      }
-      let flag = true
-      for (let j = 0; j < res[i].length; j++) {
-        if (res[i][j] !== path[j]) {
-          flag = false
-          break
+  const res = []; path = [], len = candidates.length;
+    candidates.sort((a,b)=>a-b);
+    backtracking(0, 0);
+    return res;
+    function backtracking(sum, i) {
+        if (sum === target) {
+          console.log('k',path)
+            res.push(Array.from(path));
+            return;
         }
-      }
-      if (flag) {
-        return false
-      }
+        for(let j = i; j < len; j++) {
+            const n = candidates[j];
+            if(j > i && candidates[j] === candidates[j-1]){
+              console.log('j',j,i,candidates[j],candidates[j-1])
+              //若当前元素和前一个元素相等
+              //则本次循环结束，防止出现重复组合
+              continue;
+            }
+            //如果当前元素值大于目标值-总和的值
+            //由于数组已排序，那么该元素之后的元素必定不满足条件
+            //直接终止当前层的递归
+            if(n > target - sum) break;
+            path.push(n);
+            sum += n;
+            backtracking(sum, j + 1);
+            path.pop();
+            sum -= n;
+        }
     }
-    return true
-  }
-  function backTrack(start, sum) {
-    if (sum > target) {
-      return
-    }
-    if (sum === target) {
-      const flag = notExistRes(path)
-      if (flag) {
-        res.push([...path])
-      }
-      return
-    }
-    for (let i = start; i < candidates.length; i++) {
-      const num = candidates[i]
-      path.push(num)
-      sum += num
-      backTrack(i + 1, sum)
-      sum -= path.pop()
-    }
-  }
-  backTrack(0, 0)
-  return res
 };
 // @lc code=end
 

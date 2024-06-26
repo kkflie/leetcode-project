e17f11495aa01bc4de86d18ec84594d6b73ab471
gyb <gyb984571377@gmail.com>
2024-06-26 17:07:38 +0800

    [199] 二叉树的右视图

---
+++ src/199.binary-tree-right-side-view.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=javascript
+ * @lcpr version=30204
+ *
+ * [199] 二叉树的右视图
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+  if (!root) return []
+  const res = []
+  const queue = [root]
+  while(queue.length) {
+    const len = queue.length
+    res.push(queue[queue.length - 1].val)
+    for (let i = 0; i < len; i++) {
+      const node = queue.shift()
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+  }
+  return res
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,null,5,null,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,null,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+

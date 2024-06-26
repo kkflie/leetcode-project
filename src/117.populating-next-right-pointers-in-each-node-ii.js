f2aa65b94f3d68b036b0c5584eb376e2bd239d8b
gyb <gyb984571377@gmail.com>
2024-06-26 17:30:30 +0800

    [117] 填充每个节点的下一个右侧节点指针 II

---
+++ src/117.populating-next-right-pointers-in-each-node-ii.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=javascript
+ * @lcpr version=30204
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function(root) {
+  if (!root) return root
+  const queue = [root]
+  while (queue.length) {
+    const len = queue.length
+    for (let i = 0; i < len; i++) {
+      const node = queue.shift()
+      if (i + 1 < len) {
+        node.next = queue[0]
+      }
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+  }
+  return root 
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5,null,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+

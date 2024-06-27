c9c26eb8b49f0a6751edfaa1785721feb0c82502
gyb <gyb984571377@gmail.com>
2024-06-27 17:40:14 +0800

    [669] 修剪二叉搜索树-迭代

--- src/669.trim-a-binary-search-tree.js
+++ src/669.trim-a-binary-search-tree.js
@@ -27,14 +27,27 @@
 var trimBST = function(root, low, high) {
   // review
   if (root === null) return null
-  if (root.val < low) {
-    return trimBST(root.right, low, high)
+  while (root !== null && (root.val < low || root.val > high)) {
+    if (root.val < low) {
+      root = root.right
+    } else {
+      root = root.left
+    }
   }
-  if (root.val > high) {
-    return trimBST(root.left, low, high)
+  let cur = root
+  while(cur) {
+    while(cur.left && cur.left.val < low) {
+      cur.left = cur.left.right
+    }
+    cur = cur.left
   }
-  root.left = trimBST(root.left, low, high)
-  root.right = trimBST(root.right, low, high)
+  cur = root
+  while(cur) {
+    while(cur.right && cur.right.val > high) {
+      cur.right = cur.right.left
+    }
+    cur = cur.right
+  }
   return root
 };
 // @lc code=end

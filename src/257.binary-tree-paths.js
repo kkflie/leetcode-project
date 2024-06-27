7edc7e483f1b9dc0bbfa2e2e9813d64ac4f19196
gyb <gyb984571377@gmail.com>
2024-06-27 11:15:46 +0800

    [257] 二叉树的所有路径

---
+++ src/257.binary-tree-paths.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=257 lang=javascript
+ * @lcpr version=30204
+ *
+ * [257] 二叉树的所有路径
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+  if (!root) return []
+  const res = []
+  function traverse(line, root) {
+    if (!root.left && !root.right) {
+      res.push(line.concat(root.val).join('->'))
+      return
+    }
+    root.right && traverse(line.concat(root.val), root.right)
+    root.left && traverse(line.concat(root.val), root.left)
+  }
+  traverse([], root)
+  return res
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,null,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+

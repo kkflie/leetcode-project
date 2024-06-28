12d1e171d9a0924cb717f5d5d5f32bf187e87901
gyb <gyb984571377@gmail.com>
2024-06-28 11:27:38 +0800

    [63] 不同路径 II-一维数组

--- src/63.unique-paths-ii.js
+++ src/63.unique-paths-ii.js
@@ -15,24 +15,29 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function(obstacleGrid) {
-  const dp = obstacleGrid
-  const m = dp.length
-  const n = dp[0].length
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j< n; j++) {
-      if (dp[i][j] === 1) {
-        dp[i][j] = 0
-      } else if (i===0) {
-        dp[i][j] = j === 0 ? 1 : dp[i][j-1]
-      } else if (j === 0) {
-        dp[i][j] = dp[i-1][j]
+  const m = obstacleGrid.length
+  const n = obstacleGrid[0].length
+  const dp = new Array(n).fill(1)
+  for (let j = 0; j < n; j++) {
+    if (obstacleGrid[0][j] === 1) {
+      dp[j] = 0
+    } else {
+      dp[j] = j === 0 ? 1 : dp[j - 1]
+    }
+  }
+  for (let i = 1; i < m; i++) {
+    if (obstacleGrid[i][0] === 1) {
+      dp[0] = 0
+    }
+    for (let j = 1; j < n; j++) {
+      if (obstacleGrid[i][j] === 1) {
+        dp[j] = 0
       } else {
-        dp[i][j] = dp[i - 1][j] + dp[i][j-1]
+        dp[j] += dp[j - 1]
       }
     }
   }
-  // console.log(dp)
-  return dp[m-1][n-1]
+  return dp[n - 1]
 };
 // @lc code=end
 

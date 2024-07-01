737a3d14583cb3706ae4f3c46f6faf33c4d6e96d
gyb <gyb984571377@gmail.com>
2024-07-01 12:28:46 +0800

    [123] 买卖股票的最佳时机 III

---
+++ src/123.best-time-to-buy-and-sell-stock-iii.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=123 lang=javascript
+ * @lcpr version=30204
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  // review
+  const dp = new Array(5).fill(0)
+  dp[1] = - prices[0]
+  dp[3] = - prices[0]
+  for (let i = 1; i < prices.length; i++) {
+    dp[1] = Math.max(dp[1], dp[0] - prices[i])
+    dp[2] = Math.max(dp[2], dp[1] + prices[i])
+    dp[3] = Math.max(dp[3], dp[2] - prices[i])
+    dp[4] = Math.max(dp[4], dp[3] + prices[i])
+  }
+  return dp[4]
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,3,5,0,0,3,1,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [7,6,4,3,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+

94c7b32a72098a2e2c8e4f6fed3c5744601a68c7
gyb <gyb984571377@gmail.com>
2024-07-03 09:56:14 +0800

    复习

--- src/332.reconstruct-itinerary.js
+++ src/332.reconstruct-itinerary.js
@@ -16,7 +16,6 @@
  */
 var findItinerary = function(tickets) {
   let map = {}
-  let res = []
   const path = ['JFK']
   tickets.forEach(([start, end]) => {
     if (!map[start]) {
@@ -26,23 +25,16 @@ var findItinerary = function(tickets) {
     value[end] = (value[end] || 0) + 1
     map[start] = value
   })
-  // map = sortObj(map)
-  // console.log(map)
   function backTrack(start) {
     if (path.length === tickets.length + 1) {
-      res = path
       return true
     }
     const endList = Object.entries(map[start] || {}).filter((e) => e[1] > 0)
-    // console.log(endList, start, endList.length)
-    // if (endList.length < 1) {
     if (endList.length < 1) {
       return false
     }
-      // console.log('end0', endList[0])
     endList.sort((a, b) => a[0] < b[0] ? -1 : 1)
     for (let i = 0; i < endList.length; i++) {
-      // console.log('end0', endList[0])
       const end = endList[i]
       path.push(end[0])
       map[start][end[0]]--
@@ -52,22 +44,8 @@ var findItinerary = function(tickets) {
     }
     return false
   }
-  // function sortObj(obj) {
-  //   let newObj = {}
-  //   const keys = Object.keys(obj)
-  //   keys.sort((a, b) => a > b ? 1 : -1)
-  //   keys.forEach((key) => {
-  //     if (obj[key] !== null && typeof obj[key] === 'object') {
-  //       newObj[key] = sortObj(obj[key])
-  //     } else {
-  //       newObj[key] = obj[key]
-  //     }
-  //   })
-  //   return newObj
-  // }
   backTrack('JFK')
-  // console.log(res)
-  return res
+  return path
 };
 // @lc code=end
 
